7abf73da5fc3822e4e27a9c1e844c50c106c10c8
Nicole Martin <nikismar@hotmail.com>
2017-07-02 15:26:43 -0500

    got add to work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,30 @@
 console.log("js sourced");
+
+//document ready function
+$(document).ready(function() {
+console.log('jq sourced');
+
+
+// creating object with data to be used for the calcuation in the ajax POST request
+$('#calcButton').on('click', function(){
+  var objectToSend = {
+   input1 : $('#input1').val(),
+   input2 : $('#input2').val(),
+   type : $('#add').text()
+}; //end of objectToSend
+console.log(objectToSend);
+
+
+//sending data from inputs (objectToSend) to the server to do the calculation.
+  $.ajax({
+    type: 'POST',
+    url: '/calculation',
+    data: objectToSend,
+    success: function(response) {
+      console.log('input sent to server be calculated');
+      $('#total').val(response.calculation);
+    }
+
+});//end of on click function for calcButton
+  }); //end of ajax POST
+}); // end of document Ready
--- server/server.js
+++ server/server.js
@@ -4,6 +4,25 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var port = 5656;
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded( {extended: true}));
+
+
+//Server-side POST information recieves the data from objectToSend on the client
+// side and uses it to calculate the total based on the objectToSend information.
+//
+app.post ('/calculation', function(req, res){
+  console.log('Input recieved', req.body);
+  //var calcInput = req.body; //This is the data we send (input1, input2)
+var responseObject = {
+  calculation: math(req.body)
+};
+  // console.log(product);
+  //calcInput.push(product);
+  res.send(responseObject);
+
+ });
+
 
 app.get('/*', function(req, res) {
   var file = req.params[0] || 'views/index.html';
@@ -13,3 +32,21 @@ app.get('/*', function(req, res) {
 app.listen(port, function(){
   console.log('Server running on port', port);
 });
+
+
+// checks to see with operator is being used and returns calcuation using a
+// switch statement to determine math to be done.
+var math = function(object) {
+  switch (object.type) {
+    case '+':
+      return Number(object.input1) + Number(object.input2);
+    case '-':
+      return Number(object.input1) - Number(object.input2);
+    case '*':
+      return Number(object.input1) * Number(object.input2);
+    case '/':
+      return Number(object.input1) / Number(object.input2);
+    default:
+      break;
+  } //end of switch statement
+}; //end of math function
