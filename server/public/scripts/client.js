110843c406ba0d24437866a39a301af6d2643cc3
Nicole Martin <nikismar@hotmail.com>
2017-07-02 16:13:16 -0500

    base mode complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,16 +1,25 @@
 console.log("js sourced");
+// var mathOperator;
 
 //document ready function
 $(document).ready(function() {
 console.log('jq sourced');
 
+// $('.operators').on('click', function() {
+//   switch()
+//     case 'add':
+//       mathOperator = '+';
+//       break;
+//     case 'subtract':
+//
+// });
 
 // creating object with data to be used for the calcuation in the ajax POST request
-$('#calcButton').on('click', function(){
+$('.operators').on('click', function(){
   var objectToSend = {
    input1 : $('#input1').val(),
    input2 : $('#input2').val(),
-   type : $('#add').text()
+   type : $(this).text()
 }; //end of objectToSend
 console.log(objectToSend);
 
@@ -24,7 +33,13 @@ console.log(objectToSend);
       console.log('input sent to server be calculated');
       $('#total').val(response.calculation);
     }
-
 });//end of on click function for calcButton
   }); //end of ajax POST
+
+//clearing inputs
+$('#clearButton').on('click', function(){
+  $('#input1').val('');
+  $('#input2').val('');
+  $('#total').val('');
+}); //end of on click of clearButton
 }); // end of document Ready
